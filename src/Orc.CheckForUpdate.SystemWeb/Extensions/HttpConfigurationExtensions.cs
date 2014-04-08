9f26c3fb6d27fbecb5e7d829d5f252b57f642361
auz34 <andrewzavgorodniy@gmail.com>
2014-04-08 08:57:10 +0300

    Commit with a structure

---
+++ src/Orc.CheckForUpdate.SystemWeb/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Orc.CheckForUpdate.SystemWeb.Extensions
+{
+    using System.Web.Http;
+
+    public static class HttpConfigurationExtensions
+    {
+        public static void MapVersionApi(this HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "VersionApi",
+                routeTemplate: "verapi/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}

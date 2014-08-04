8953135298a16747a7112d7cd84fc420b20c57f7
auz34 <andrewzavgorodniy@gmail.com>
2014-08-04 10:34:15 -0700

    Defining interfaces for versioning logic on the .net 4.0 client

---
+++ src/Orc.CheckForUpdate.Client.Net40/Abstract/IVersioningService.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IVersioningService.cs" company="ORC">
+//   MS-PL
+// </copyright>
+// <summary>
+//   Interface defining basic versioning operations
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orc.CheckForUpdate.Client
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface defining basic versioning operations.
+    /// </summary>
+    public interface IVersioningService
+    {
+        /// <summary>
+        /// Gets current version.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        string GetCurrentVersion();
+
+        /// <summary>
+        /// Checks whether update available.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> of checking whether update available.
+        /// </returns>
+        Task IsUpdateAvailable();
+
+        /// <summary>
+        /// The get latest version.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> of getting latest version number.
+        /// </returns>
+        Task GetLatestVersion();
+
+        /// <summary>
+        /// The download version.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> of downloading version.
+        /// </returns>
+        Task DownloadVersion();
+    }
+}
---
+++ src/Orc.CheckForUpdate.Client.Net40/Abstract/IVersioningSettingsService.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IVersioningSettingsService.cs" company="ORC">
+//   MS-PL
+// </copyright>
+// <summary>
+//   Defines the IVersioningSettingsService type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orc.CheckForUpdate.Client
+{
+    /// <summary>
+    /// The VersioningSettingsService interface.
+    /// </summary>
+    public interface IVersioningSettingsService
+    {
+        /// <summary>
+        /// Gets a value indicating whether unstable versions.
+        /// </summary>
+        bool CheckForUnstableVersions { get; }
+
+        /// <summary>
+        /// Gets the versions api url.
+        /// </summary>
+        string VersionsApiUrl { get; }
+    }
+}

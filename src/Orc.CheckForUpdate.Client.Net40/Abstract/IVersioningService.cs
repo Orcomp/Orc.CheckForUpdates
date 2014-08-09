bd723bf4cdb2be33e836e20f2bebaf073951c45d
auz34 <andrewzavgorodniy@gmail.com>
2014-08-09 10:39:33 -0700

    workflow design revised

--- src/Orc.CheckForUpdate.Client.Net40/Abstract/IVersioningService.cs
+++ src/Orc.CheckForUpdate.Client.Net40/Abstract/IVersioningService.cs
@@ -9,6 +9,7 @@
 
 namespace Orc.CheckForUpdate.Client
 {
+    using System;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -30,7 +31,7 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of checking whether update available.
         /// </returns>
-        Task IsUpdateAvailable();
+        Task<bool> IsUpdateAvailable();
 
         /// <summary>
         /// The get latest version.
@@ -38,7 +39,7 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of getting latest version number.
         /// </returns>
-        Task GetLatestVersion();
+        Task<string> GetLatestVersion();
 
         /// <summary>
         /// The download version.
@@ -46,6 +47,22 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of downloading version.
         /// </returns>
-        Task DownloadVersion();
+        Task<string> DownloadVersion();
+
+        /// <summary>
+        /// Starts workflow in which
+        /// a) gets the latest version number from the server;
+        /// b) compares the latest version number with current one;
+        /// c) *if needed* downloads latest version installer from the server;
+        /// d) *if needed* executes action to determine whether latest version installer should be started;
+        /// e) *if needed* starts latest version installer.
+        /// </summary>
+        /// <param name="shouldStartInsaller">
+        /// The function which helps to determine whether latest version installer should be started.
+        /// </param>
+        /// <returns>
+        /// The workflow <see cref="Task"/> .
+        /// </returns>
+        Task<string> StartCheckWorkflow(Func<string, bool> shouldStartInsaller);
     }
 }
--- src/Orc.CheckForUpdate.Client.Net40/Implementations/VersioningService.cs
+++ src/Orc.CheckForUpdate.Client.Net40/Implementations/VersioningService.cs
@@ -9,6 +9,7 @@
 
 namespace Orc.CheckForUpdate.Client
 {
+    using System;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -46,7 +47,7 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of checking whether update available.
         /// </returns>
-        public Task IsUpdateAvailable()
+        public Task<bool> IsUpdateAvailable()
         {
             throw new System.NotImplementedException();
         }
@@ -57,7 +58,7 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of getting latest version number.
         /// </returns>
-        public Task GetLatestVersion()
+        public Task<string> GetLatestVersion()
         {
             throw new System.NotImplementedException();
         }
@@ -68,7 +69,26 @@ namespace Orc.CheckForUpdate.Client
         /// <returns>
         /// The <see cref="Task"/> of downloading version.
         /// </returns>
-        public Task DownloadVersion()
+        public Task<string> DownloadVersion()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Starts workflow in which
+        /// a) gets the latest version number from the server;
+        /// b) compares the latest version number with current one;
+        /// c) *if needed* downloads latest version installer from the server;
+        /// d) *if needed* executes action to determine whether latest version installer should be started;
+        /// e) *if needed* starts latest version installer.
+        /// </summary>
+        /// <param name="shouldStartInsaller">
+        /// The function which helps to determine whether latest version installer should be started.
+        /// </param>
+        /// <returns>
+        /// The workflow <see cref="Task"/> .
+        /// </returns>
+        public Task<string> StartCheckWorkflow(Func<string, bool> shouldStartInsaller)
         {
             throw new System.NotImplementedException();
         }
